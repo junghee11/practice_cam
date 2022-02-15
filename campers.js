fb22a85cb665020d5dbb7786a33747c16f164d16
junghee11 <namjunghee11@naver.com>
2022-02-15 14:17:43 +0900

    22.02.15

--- campers.js
+++ campers.js
@@ -1,7 +1,37 @@
 
 
-document.addEventListener(scroll,function(){
+document.addEventListener('scroll', function(){
+    var pos = document.documentElement.scrollTop;
+    var modal = document.getElementById('main-header');
+    if(pos>100){
+        modal.style.position="fixed";
+        modal.style.top="0px";
+        modal.style.left="0px";
+        modal.style.width="100%";
+    } else {
+        modal.style.position = "static";
+    }
+});
 
+window.addEventListener('resize', function(){
+    var width = window.innerWidth;
+    var nav = document.getElementById('header-nav');
+    var mobile = document.getElementById("mobile_menu");
+    var top = document.getElementById("top-until");
+    if(width>1270){
+        nav.style.display = "block";
+        mobile.style.display="none";
+        
+    } else if(width>900) {
+        nav.style.display="none";
+        top.style.display="flex";
+        mobile.style.display="none";
+       
+    } else {
+        nav.style.display="none";
+        mobile.style.display="block";
+        top.style.display="none";
+    }
 });
 function sub_menu(){
     var sub = document.getElementsByClassName("sub-menu-inner");
@@ -17,4 +47,7 @@ function close_menu(){
         sub[i].style.display = "none";
     }
     
+}
+function open_modal(){
+
 }
